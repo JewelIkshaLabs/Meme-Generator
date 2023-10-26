caa5314658ecce8b7e2dced00531fdc95dfc1459
Jewel-Thomas <jewelthomas20@gmail.com>
2023-10-26 19:52:28 +0530

    change it

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import Header from "./Components/Header";
+import Meme from "./Components/Meme";
+
+export default function App() {
+    return (
+      <div>
+        <Header />
+        <Meme />
+      </div>
+    );
+}
